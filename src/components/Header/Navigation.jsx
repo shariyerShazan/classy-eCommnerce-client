997e5fa8c1b6109dfb44a379ffee02f79e67f635
shariyerShazan <shariyershazan1@gmail.com>
2025-09-19 01:50:16 +0600

    drawer category done

--- src/components/Header/Navigation.jsx
+++ src/components/Header/Navigation.jsx
@@ -1,16 +1,22 @@
 import Button from '@mui/material/Button'
-import React from 'react'
+import React, { useState } from 'react'
 import { CgMenuLeftAlt } from "react-icons/cg";
 import { TfiAngleDown } from "react-icons/tfi";
 import { Link } from 'react-router';
 import { HiOutlineRocketLaunch } from "react-icons/hi2";
+import CategoryDrawer from './CategoryDrawer';
 
 const Navigation = () => {
+    const [openCtg , setOpenCtg] = useState(false)
+    const openCategory = ()=>{
+        setOpenCtg((prev)=> !prev)
+    }
   return (
-    <nav className='border-b-1 border-gray-300'>
+    <>
+    <nav  className='border-b-1 border-gray-300'>
         <div className='container flex items-center justify-between'>
              <div className='col-1 py-2 flex'>
-                   <Button className='!text-black   !flex !gap-2 '>
+                   <Button onClick={openCategory} className='!text-black   !flex !gap-2 '>
                    <CgMenuLeftAlt size={20}/>
                       Shop By Categories
                       <TfiAngleDown className='ml-12' size={20}/>
@@ -23,39 +29,39 @@ const Navigation = () => {
                    <div className='categories'>
                     <ul className="h-full flex gap-2">
                           <li className='list-none'>
-                               <Button  sx={{ textTransform: 'capitalize' }}  component={Link}    to="/" disableElevation   className="!text-sm !text-black !py-4 !px-2" > Home </Button> 
+                               <Button  sx={{ textTransform: 'capitalize' }}  component={Link}    to="/" disableElevation   className="!text-sm !text-black hover:!text-primary !transition-all !duration-300 !py-4 !px-2" > Home </Button> 
                           </li> 
 
                           <li className='list-none'>
-                               <Button  sx={{ textTransform: 'capitalize' }}  component={Link}    to="/" disableElevation   className="!text-sm !text-black !py-4 !px-2" > Fashion </Button> 
+                               <Button  sx={{ textTransform: 'capitalize' }}  component={Link}    to="/" disableElevation   className="!text-sm !text-black hover:!text-primary !transition-all !duration-300 !py-4 !px-2" > Fashion </Button> 
                           </li> 
 
                           <li className='list-none'>
-                               <Button sx={{ textTransform: 'capitalize' }}  component={Link}    to="/" disableElevation   className="!text-sm !text-black !py-4 !px-2" > Electronics </Button> 
+                               <Button sx={{ textTransform: 'capitalize' }}  component={Link}    to="/" disableElevation   className="!text-sm !text-black hover:!text-primary !transition-all !duration-300 !py-4 !px-2" > Electronics </Button> 
                           </li> 
 
                           <li className='list-none'>
-                               <Button  sx={{ textTransform: 'capitalize' }}   component={Link}    to="/" disableElevation   className="!text-sm !text-black !py-4 !px-2" > Bags </Button> 
+                               <Button  sx={{ textTransform: 'capitalize' }}   component={Link}    to="/" disableElevation   className="!text-sm !text-black hover:!text-primary !transition-all !duration-300 !py-4 !px-2" > Bags </Button> 
                           </li> 
 
                           <li className='list-none'>
-                               <Button  sx={{ textTransform: 'capitalize' }}   component={Link}    to="/" disableElevation   className="!text-sm !text-black !py-4 !px-2" > Footwear </Button> 
+                               <Button  sx={{ textTransform: 'capitalize' }}   component={Link}    to="/" disableElevation   className="!text-sm !text-black hover:!text-primary !transition-all !duration-300 !py-4 !px-2" > Footwear </Button> 
                           </li> 
 
                           <li className='list-none'>
-                               <Button  sx={{ textTransform: 'capitalize' }}   component={Link}    to="/" disableElevation   className="!text-sm !text-black !py-4 !px-2" > Groceries </Button> 
+                               <Button  sx={{ textTransform: 'capitalize' }}   component={Link}    to="/" disableElevation   className="!text-sm !text-black hover:!text-primary !transition-all !duration-300 !py-4 !px-2" > Groceries </Button> 
                           </li> 
 
                           <li className='list-none'>
-                               <Button  sx={{ textTransform: 'capitalize' }}   component={Link}    to="/" disableElevation   className="!text-sm !text-black !py-4 !px-2" > Beauty </Button> 
+                               <Button  sx={{ textTransform: 'capitalize' }}   component={Link}    to="/" disableElevation   className="!text-sm !text-black hover:!text-primary !transition-all !duration-300 !py-4 !px-2" > Beauty </Button> 
                           </li> 
 
                           <li className='list-none'>
-                               <Button  sx={{ textTransform: 'capitalize' }}   component={Link}    to="/" disableElevation   className="!text-sm !text-black !py-4 !px-2" > Wellness </Button> 
+                               <Button  sx={{ textTransform: 'capitalize' }}   component={Link}    to="/" disableElevation   className="!text-sm !text-black hover:!text-primary !transition-all !duration-300 !py-4 !px-2" > Wellness </Button> 
                           </li> 
 
                           <li className='list-none'>
-                               <Button  sx={{ textTransform: 'capitalize' }}   component={Link}    to="/" disableElevation   className="!text-sm !text-black !py-4 !px-2" > Jewellery </Button> 
+                               <Button  sx={{ textTransform: 'capitalize' }}   component={Link}    to="/" disableElevation   className="!text-sm !text-black hover:!text-primary !transition-all !duration-300 !py-4 !px-2" > Jewellery </Button> 
                           </li> 
                      </ul>
              </div>
@@ -66,6 +72,8 @@ const Navigation = () => {
              </div>
         </div>
     </nav>
+    <CategoryDrawer openCtg={openCtg} setOpenCtg={setOpenCtg}/>
+ </>
   )
 }
 
